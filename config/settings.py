d60e18a8a304cbe118b72412d2812b27fc3869b2
carlospc247 <carlospc247@gmail.com>
2025-10-31 09:47:12 +0100

    settings

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ifb+xun*6=-l(4$jdsd=nup^$rm#9exb2m=*6tb$dbksryds*$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
